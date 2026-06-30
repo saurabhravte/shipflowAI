5ca9868fed8f2f75ff98288038aff608020569c9
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-06-30 12:13:39 +0530

    fix(auth): ensure activeWorkspaceId is set during session creation for new users

--- apps/api/src/modules/github/github.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { requireAuth, requireWorkspaceMember } from "../../common/middleware/auth.middleware";
-import { getInstallUrl, getInstallation, listRepos } from "./github.controller";
-
-export const githubRoutes = Router();
-githubRoutes.use(requireAuth, requireWorkspaceMember);
-githubRoutes.get("/install-url", getInstallUrl);
-githubRoutes.get("/installation", getInstallation);
-githubRoutes.get("/repos", listRepos);
