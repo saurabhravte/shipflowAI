ad515c6d7f966fe4dd8c18c9c3ea69f69f1cded7
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-06-30 12:13:25 +0530

    delete(debug-env): remove unused debug environment route

--- apps/web/src/app/api/debug-env/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    keyId: process.env.RAZORPAY_KEY_ID ?? null,
-    hasSecret: !!process.env.RAZORPAY_KEY_SECRET,
-  });
-}
